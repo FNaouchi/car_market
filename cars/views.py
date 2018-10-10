c6cd685538eaa6fa673eff6e47c8aeb1f44aa5f1
FNaouchi <f.naoushi@hotmail.com>
2018-10-10 15:45:20 +0300

    task submission

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +20,41 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	msg = "The car has been created."
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Car Created!!!!')
+			return redirect("car-list")
+	context = {
+		"form":form,
+		"msg": msg,
+
+	}
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	msg = "The car has been updated."
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None, instance=car_obj)
+		if form.is_valid():
+			form.save()
+			messages.add_message(request, messages.INFO, 'Car Updated!!')
+			return redirect('car-list')
+	context = {
+		"form":form,
+		"obj":car_obj,
+		"msg": msg,
+	}
+	return render(request, 'update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	Car.objects.get(id=car_id).delete()
+	messages.add_message(request, messages.INFO, 'Car Deleted!!')
+	return redirect("car-list")
